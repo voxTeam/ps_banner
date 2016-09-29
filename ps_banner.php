6d862c2efc2165f1449e3d476d0c6195869b2e24
Maxime Biloé <maxime.biloe@prestashop.com>
2016-09-29 14:36:05 +0200

    // Version updated to v1.0.2

--- ps_banner.php
+++ ps_banner.php
@@ -34,7 +34,7 @@ class Ps_Banner extends Module implements WidgetInterface
 	public function __construct()
 	{
 		$this->name = 'ps_banner';
-		$this->version = '1.0.1';
+		$this->version = '1.0.2';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
